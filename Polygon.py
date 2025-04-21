28083cb4c0246effbe9a9975cae7cbee2cfa56d0
Yarden Viktor Dejorno <hrsi56@gmail.com>
2025-04-21 20:43:54 +0300

    חדש

--- Polygon.py
+++ Polygon.py
@@ -307,7 +307,7 @@ def main():
 
     if st.checkbox("Provide internal angles?"):
         angs = [st.number_input(
-            f"∠ {vertex_names(n)[i]}", 1.0, 179.0,
+            f"∠ {vertex_names(n)[i]}", 1.0, 360.0,
             round(180 * (n - 2) / n, 1), 1.0, key=f"A{i}"
         ) for i in range(n)]
         poly = build_polygon(lengths, repaired_angles(n, angs))
