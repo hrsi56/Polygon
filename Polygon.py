dd083e3b06d8f30b933e7fa475c9d6a971bf07b7
Yarden Viktor Dejorno <hrsi56@gmail.com>
2025-04-21 19:46:44 +0300

    חדש

--- Polygon.py
+++ Polygon.py
@@ -286,6 +286,8 @@ def draw_polygon(poly: PolygonData):
             ha="center", va="bottom")
     ax.text(*mid_h, f"h={h:.2f}", fontsize=8,
             ha="left", va="center")
+    ax.text(*[0,0], f"Area={(h*w):.2f}", fontsize=8,
+            ha="left", va="center")
 
     return fig, diags
 
