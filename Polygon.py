76e13e3c65b9709e5f2e4cb96360c31d78df5b1f
Yarden Viktor Dejorno <hrsi56@gmail.com>
2025-04-22 09:50:41 +0300

    חדש

--- Polygon.py
+++ Polygon.py
@@ -329,19 +329,19 @@ def draw_polygon(poly: PolygonData, show_altitudes: bool):
             ha="center", va="bottom")
     ax.text(*mid_h, f"h={h:.2f}", fontsize=8,
             ha="left", va="center")
-    ax.text(*(rect[0]-0.2), f"Area REC={HW:.2f}", fontsize=8,
+    ax.text(*(rect[0]-[0,0.2]), f"Area REC={HW:.2f}", fontsize=8,
             ha="left", va="center")
-
-    return fig, diags, altitudes_data
 
 
     # ----- area label ------------------------------------------------------
-    ax.text(*(rect[0]-0.1),
+    ax.text(*(rect[0]-[0,0.1]),
             f"Area Poligon = {shoelace_area(poly.pts):.2f}",
             fontsize=9, color="green",
             ha="center", va="center",
             bbox=dict(facecolor="white", alpha=0.7,
                       edgecolor="none"))
+
+    return fig, diags, altitudes_data
 
 # ────── Streamlit UI ───────────────────────────────────────────────────────
 def main():
